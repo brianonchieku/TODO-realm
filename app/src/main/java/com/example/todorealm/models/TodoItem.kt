ec3740661173a679eace2b9505fbf868d126c960
brianonchieku <brianonchikeku0@gmail.com>
2024-12-09 06:18:07 +0300

    first commit

---
+++ app/src/main/java/com/example/todorealm/models/TodoItem.kt
@@ -0,0 +1,12 @@
+package com.example.todorealm.models
+
+import io.realm.kotlin.types.RealmObject
+import io.realm.kotlin.types.annotations.PrimaryKey
+import org.mongodb.kbson.ObjectId
+
+class TodoItem: RealmObject {
+    @PrimaryKey val _id: ObjectId = ObjectId()
+    var description: String = ""
+    var isCompleted: Boolean = false
+
+}
