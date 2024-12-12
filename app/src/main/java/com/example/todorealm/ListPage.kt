777d2785da7989e9df53fe8cdbee5cbe1eb68939
brianonchieku <brianonchikeku0@gmail.com>
2024-12-12 13:13:24 +0300

    main screen

---
+++ app/src/main/java/com/example/todorealm/ListPage.kt
@@ -0,0 +1,52 @@
+package com.example.todorealm
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ListPage(viewModel: TodoViewModel){
+    var inputText by remember { mutableStateOf("") }
+    var isComplete by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(8.dp)
+            .padding(top = 40.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            OutlinedTextField(value = inputText, onValueChange ={inputText = it},
+                modifier = Modifier.weight(1f))
+
+            Button(onClick = {
+                viewModel.createEntry(inputText, isComplete)
+            }) {
+                Text(text = "ADD")
+
+            }
+
+        }
+
+    }
+
+
+}
