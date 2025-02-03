41680ea7843a66013a1f10507c17bb34deaf4ba3
brianonchieku <brianonchikeku0@gmail.com>
2025-02-03 18:31:20 +0300

    viewmodel

--- app/src/main/java/com/example/todorealm/ListPage.kt
+++ app/src/main/java/com/example/todorealm/ListPage.kt
@@ -2,16 +2,19 @@ package com.example.todorealm
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
@@ -22,9 +25,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import com.example.todorealm.models.TodoItem
 
 @Composable
@@ -58,25 +63,36 @@ fun ListPage(viewModel: TodoViewModel){
 
         }
 
-        todos.let {
-            LazyColumn(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.spacedBy(16.dp)
-            ) {
-                items(todos){ item ->
-                    TodoItem(item = item,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(16.dp)
-                            .clickable {
-                                viewModel.showtodoItems(item)
-                            })
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp), contentAlignment = Alignment.Center){
+
+            if(viewModel.todoItem != null){
+
+            }
+
+            todos.let {
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.spacedBy(16.dp)
+                ) {
+                    items(todos){ item ->
+                        TodoItem(item = item,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp)
+                                .clickable {
+                                    viewModel.showtodoItems(item)
+                                })
+
+                    }
 
                 }
 
             }
-
         }
+
+
 
     }
 }
@@ -95,6 +111,25 @@ fun TodoItem(item: TodoItem, modifier: Modifier){
         }
 
     }
+}
 
+@Composable
+fun AddDialog(item: TodoItem){
+    Dialog(onDismissRequest = { /*TODO*/ }) {
+        Card(
+            modifier = Modifier
+                .wrapContentSize()
+                .padding(16.dp)
+        ) {
+            Column(
+                modifier = Modifier.padding(16.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                Text(text = item.description, fontSize = 30.sp)
 
+            }
+
+        }
+
+    }
 }
